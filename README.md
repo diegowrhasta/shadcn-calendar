51f4680e82cbc94aa599d003e639e9dddf72d5b3
Diego Balderrama <diegowrhasta@gmail.com>
2025-07-12 15:32:54 +0900

    Adds shadcn, configures it, and adds customized calendar

--- README.md
+++ README.md
@@ -17,6 +17,37 @@ were:
 - Used `pnpm`. So installation is with `pnpm install`
 - `pnpm run dev`
 
+### Shadcn
+
+Shadcn is a library built in order to allow developers to import components surgically 
+and build their own component libraries it's basically an "unstylized component 
+library". It allows for one to add already set-up components that can be then extended 
+both in styling and functionality.
+
+#### Setup
+
+Reference: https://ui.shadcn.com/docs/installation/vite[DOCS].
+
+Due to the fact that we have setup our project with _vite_ we will be using shadcn's 
+docs to set the project up correctly and import its calendar.
+
+**Key Takeaways**
+
+.You need to add Tailwind CSS
+.Replace the index.css
+.Edit the typescript files
+.Update `vite.config.ts`
+.Run shadcn's CLI
+.Profit with components!
+
+#### Adding calendar
+
+.`pnpm dlx shadcn@latest add calendar`
+
+### Errors and solutions
+
+
+
 ## Takeaways
 
 <!-- - Shadcn helps getting an already working component to stylize to one's liking pretty easily.
--- shadcn-calendar/src/App.tsx
+++ shadcn-calendar/src/App.tsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Calendar } from "@/components/ui/calendar"
+
 import './App.css'
+import { useState } from "react"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [date, setDate] = useState<Date | undefined>(new Date())
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="w-full px-8">
+      <div className="flex gap-x-2">
+        <div className="flex-1 h-[650px] w-full bg-blue-200">
+          <Calendar
+            mode="single"
+            selected={date}
+            onSelect={setDate}
+            className="h-full w-full flex"
+            classNames={{
+              months:
+                "flex w-full flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0 flex-1",
+              month: "space-y-4 w-full flex flex-col",
+              table: "w-full h-full border-collapse space-y-1",
+              row: "w-full mt-2",
+            }}
+          />
+        </div>
+        <div className="flex-1"></div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
-export default App
+export { App }
